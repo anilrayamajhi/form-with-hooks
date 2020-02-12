ece20872feb532efce49454994066c9eeff46021
~A <arayamajhi@endertech.com>
2020-02-12 14:48:50 -0800

    For set up endpoints

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+import useFormValidation from "./useFormValidation";
+import validateAuth from "./validateAuth";
+
+const INITIAL_STATE = {
+  username: "GI",
+  email: "gi@gi.io",
+  password: "gigigigi"
+};
+
+function Register() {
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    values,
+    errors,
+    isSubmitting
+  } = useFormValidation(INITIAL_STATE, validateAuth);
+  // const [email, setEmail] = React.useState("");
+  // const [password, setPassword] = React.useState("");
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.username}
+          name="username"
+          placeholder="Username"
+        />
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name="email"
+          value={values.email}
+          className={errors.email && "error-input"}
+          autoComplete="off"
+          placeholder="Your email address"
+        />
+        {/* {errors.email && <p className="error-text">{errors.email}</p>} */}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          className={errors.password && "error-input"}
+          name="password"
+          type="password"
+          placeholder="Choose a safe password"
+        />
+        {/* {errors.password && <p className="error-text">{errors.password}</p>} */}
+        <div>
+          <button disabled={isSubmitting} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Register />, rootElement);
